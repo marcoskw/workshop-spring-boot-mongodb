65b67a03ff13dd67c42abc6c7237e4aa60c2710a
Marcos Korbes Werle <marcoskorbeswerle@hotmail.com>
2020-08-28 14:50:38 -0300

    Getting user by id

---
+++ src/main/java/com/mkwinformatica/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.mkwinformatica.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+}
