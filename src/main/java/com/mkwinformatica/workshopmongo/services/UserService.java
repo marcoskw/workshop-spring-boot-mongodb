c7fd178a0a0ae92f61b83238c39f621c4ac1a0d6
Marcos Korbes Werle <marcoskorbeswerle@hotmail.com>
2020-08-29 12:06:13 -0300

    user update with PUT method

--- src/main/java/com/mkwinformatica/workshopmongo/services/UserService.java
+++ src/main/java/com/mkwinformatica/workshopmongo/services/UserService.java
@@ -13,29 +13,40 @@ import com.mkwinformatica.workshopmongo.services.exception.ObjectNotFoundExcepti
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
+
 	public List<User> findAll() {
-		return repository.findAll();	
+		return repository.findAll();
 	}
-	
+
 	public User findById(String id) {
-		Optional<User> obj = repository.findById(id); 
+		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
+
 	public User insert(User obj) {
 		return repository.insert(obj);
 	}
 
-	
 	public void delete(String id) {
 		findById(id);
 		repository.deleteById(id);
 	}
-		
+
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repository.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+
+	}
+
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
