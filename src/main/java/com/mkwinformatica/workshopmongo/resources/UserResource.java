36b771e389a2f9f7f815845b449f7becbded49ca
Marcos Korbes Werle <marcoskorbeswerle@hotmail.com>
2020-08-31 17:57:00 -0300

    Endpoint to return a user's posts

--- src/main/java/com/mkwinformatica/workshopmongo/resources/UserResource.java
+++ src/main/java/com/mkwinformatica/workshopmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.mkwinformatica.workshopmongo.domain.Post;
 import com.mkwinformatica.workshopmongo.domain.User;
 import com.mkwinformatica.workshopmongo.dto.UserDTO;
 import com.mkwinformatica.workshopmongo.services.UserService;
@@ -50,6 +51,13 @@ public class UserResource {
 		service.delete(id); 		
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(value= "/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
+	
 	
 	@RequestMapping(value= "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Void> update(@RequestBody UserDTO objDTO, @PathVariable String id) {
